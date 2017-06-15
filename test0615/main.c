6784214f43f93717a3bebb9016f3f392115e42d2
20161104569 <2174037646@qq.com>
2017-06-15 10:37:49 +0800

    排序
    排序

--- test0615/main.c
+++ test0615/main.c
@@ -8,8 +8,23 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    int a[10],i,j,t;
+    printf("Input 10 numbers:\n");
+    for(i=0;i<10;i++)
+        scanf("%d",&a[i]);
+    printf("\n");
+    for(i=0;i<9;i++)
+        for(j=0;j<9-i;j++)
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+    printf("The sorted numbers:\n");
+    for(i=0;i<10;i++)
+        printf("%d",a[i]);
     return 0;
 }
