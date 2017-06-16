5a9d8c503f63c7687ce958cb3673a960a1df7f6f
20161104569 <2174037646@qq.com>
2017-06-16 16:30:14 +0800

    增加指针文件
    增加指针文件

--- test0615/main.c
+++ test0615/main.c
@@ -8,23 +8,49 @@
 
 #include <stdio.h>
 
-int main()
-{
-    int a[10],i,j,t;
-    printf("Input 10 numbers:\n");
-    for(i=0;i<10;i++)
-        scanf("%d",&a[i]);
-    printf("\n");
-    for(i=0;i<9;i++)
-        for(j=0;j<9-i;j++)
-            if(a[j]>a[j+1])
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // printf("Hello, World!\n");
+    //return 0;
+    int a[20];
+    int j,i,t;
+    FILE *fp1,*fp2;
+    fp1=fopen("//Users//a20161104569//Desktop//test0615//sortout.txt","w+");
+    fp2=fopen("//Users//a20161104569//Desktop//test0615//sort.txt","r+");
+    if(fp2==NULL)
+    {
+        printf("打开文件错误,要打开的文件可能不存在");
+    }
+    else
+    {
+        
+        for(i=0;i<10;i++)
+        {
+            fscanf(fp2,"%d",&a[i]);
+        }
+        for(i=0;i<10;i++)
+        {
+            for(j=i+1;j<10;j++)
             {
-                t=a[j];
-                a[j]=a[j+1];
-                a[j+1]=t;
+                if(a[i]>a[j])
+                {
+                    t=a[i];
+                    a[i]=a[j];
+                    a[j]=t;
+                }
             }
-    printf("The sorted numbers:\n");
-    for(i=0;i<10;i++)
-        printf("%d",a[i]);
+        }
+        for(i=0;i<10;i++)
+        {
+            printf("%d ",a[i]);
+            fprintf(fp1,"%d ",a[i]);
+        }
+        //fprintf(fp1,"\n");
+        printf("\n");
+        
+        
+        fclose(fp1);
+        fclose(fp2);
+    }
     return 0;
 }
